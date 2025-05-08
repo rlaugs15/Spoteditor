5a2ad1f8cce6067ac316e0c5b8401001349bf855
hayanLee <hayanlee.dev@gmail.com>
2025-05-09 01:38:54 +0900

    add : 폰트 설정 및 custom theme 설정

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren } from 'react';
 
 const MainLayout = ({ children }: PropsWithChildren) => {
-  return <div>{children}</div>;
+  return <div className="h-dvh bg-pink-200 web:bg-yellow-300">{children}</div>;
 };
 
 export default MainLayout;
