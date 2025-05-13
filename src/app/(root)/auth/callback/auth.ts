5893f37d9c1a7cc091642589da2435201384b79b
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-05-13 23:56:28 +0900

    refactor: 소셜 로그인 시 유저 삽입 로직 제거
     - Supabase Trigger를 통해 public_users에 유저 정보가 자동 삽입되므로 최초 로그인 시 프리즈마로 수동 생성하던 로직 제거
     - 기존의 존재 여부 확인 후 create 방식에서 단순 조회 방식으로 변경

--- src/app/(root)/auth/callback/auth.ts
+++ src/app/(root)/auth/callback/auth.ts
@@ -8,31 +8,11 @@ export async function upsertGoogleUser(supabase: SupabaseClient<any, 'public', a
   } = await supabase.auth.getUser();
 
   if (!user) return;
-  const {
-    id,
-    email,
-    app_metadata: { provider },
-    user_metadata: { full_name, avatar_url },
-  } = user;
 
-  /* public_users에 해당 유저가 존재하는지 확인 */
-  const existingUser = await prisma.public_users.findUnique({
-    where: { user_id: id },
+  /* public_users에 저장된 유저 정보 조회 (트리거로 자동 삽입된 정보) */
+  const userProfile = await prisma.public_users.findUnique({
+    where: { user_id: user.id },
   });
 
-  if (existingUser) return;
-
-  /* 존재하지 않으면 최초 로그인으로 판단 → public_users에 유저 정보 저장 */
-  await prisma.public_users.create({
-    data: {
-      user_id: id,
-      email,
-      nickname: full_name,
-      image_url: avatar_url,
-      description: null,
-      insta_id: null,
-      provider,
-      is_deleted: false,
-    },
-  });
+  return userProfile;
 }
