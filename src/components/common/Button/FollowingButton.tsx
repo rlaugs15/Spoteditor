5691fe45d1485ab961b1e7692bb2e326915341f9
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-05-16 11:49:09 +0900

    feat: 팔로잉 버튼 컴포넌트 추가

---
+++ src/components/common/Button/FollowingButton.tsx
@@ -0,0 +1,34 @@
+import { Button } from '@/components/ui/button';
+import useFollowMutation from '@/hooks/mutations/follow/userFollowMutation';
+import useIsFollowing from '@/hooks/queries/follow/useIsFollowing';
+import { cn } from '@/lib/utils';
+
+interface FollowingButtonProps {
+  userId: string;
+  className?: string;
+}
+
+export default function FollowingButton({ userId, className = '' }: FollowingButtonProps) {
+  const { data: followStatus, isPending } = useIsFollowing(userId);
+  const isFollowing = followStatus?.isFollowing ?? false;
+
+  const { mutate, isPending: isMutating } = useFollowMutation();
+
+  const onFollowClick = () => {
+    if (isMutating || isPending) return;
+
+    mutate({ userId, isFollowing });
+  };
+
+  return (
+    <Button
+      onClick={onFollowClick}
+      variant={isFollowing ? 'ghost' : 'outline'}
+      size="s"
+      className={cn('font-medium', className)}
+      fullRounded
+    >
+      {isFollowing ? '팔로잉' : '팔로우'}
+    </Button>
+  );
+}
