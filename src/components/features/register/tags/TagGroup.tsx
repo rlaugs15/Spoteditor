026e98099f55402cd7b7521a44a7dfdd87172c04
hayanLee <hayanlee.dev@gmail.com>
2025-05-19 01:44:02 +0900

    fix : 로그 등록에 사용되는 태그 클릭되도록 수정
    - Set 사용

--- src/components/features/register/tags/TagGroup.tsx
+++ src/components/features/register/tags/TagGroup.tsx
@@ -1,6 +1,8 @@
-import { Button } from '@/components/ui/button';
+'use client';
 import { cityDistricts } from '@/constants/cityData';
 import { TAG_SETS } from '@/constants/tagData';
+import { useCallback, useState } from 'react';
+import TagButton from './TagButton';
 
 interface TagGroupProps {
   title: string;
@@ -8,15 +10,28 @@ interface TagGroupProps {
   selectedCity?: string;
 }
 const TagGroup = ({ title, type, selectedCity = '서울' }: TagGroupProps) => {
+  const [selectedTags, setSelectedTags] = useState<Set<string>>(new Set());
+  const handleClick = useCallback((tag: string) => {
+    setSelectedTags((prev) => {
+      const newSet = new Set(prev);
+      if (newSet.has(tag)) newSet.delete(tag);
+      else newSet.add(tag);
+      return newSet;
+    });
+  }, []);
+
   const tags = type === 'sigungu' ? cityDistricts[selectedCity] : TAG_SETS[type];
   return (
     <div className="mb-5">
       <h5 className="text-text-xs font-bold py-2.5">{title}</h5>
       <div className="flex flex-wrap gap-2">
         {tags.map((value: string) => (
-          <Button key={value} className="!text-text-xs font-bold">
-            {value}
-          </Button>
+          <TagButton
+            key={value}
+            value={value}
+            isSelected={selectedTags.has(value)}
+            onClick={handleClick}
+          />
         ))}
       </div>
     </div>
