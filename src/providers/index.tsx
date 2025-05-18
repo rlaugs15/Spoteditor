2f86becafc3cc2d296b3e101e1b4f9e17e46d365
hayanLee <hayanlee.dev@gmail.com>
2025-05-18 14:00:31 +0900

    move : MSWProvider 위치 변경

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,7 +1,13 @@
+import MSWProvider from './MSWProvider';
 import { QueryProvider } from './QueryProvider';
 
 /* Provider를 독립적인 파일로 관리하고, 한곳에서 모든 Provider 관리 */
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return <QueryProvider>{children}</QueryProvider>;
+  return (
+    <>
+      <MSWProvider />
+      <QueryProvider>{children}</QueryProvider>
+    </>
+  );
 }
