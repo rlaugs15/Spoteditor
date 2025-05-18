ef9f70b45b3958bd9c076d5fc37570977e5afc93
hayanLee <hayanlee.dev@gmail.com>
2025-05-18 22:48:33 +0900

    bugfix : Lottie 클라이언트 컴포넌트로 변경
    - lottie 라이브러리가 dom요소에 접근하려고 해서 ReferenceError: document is not defined 에러 발생

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,11 +1,12 @@
-import { cn } from '@/lib/utils';
-import Lottie from 'lottie-react';
 import loadingAnimation from '@/app/assets/loadingAnimation.json';
+import { cn } from '@/lib/utils';
+import dynamic from 'next/dynamic';
 
 interface LoadingProps {
   className?: string;
 }
 
+const Lottie = dynamic(() => import('lottie-react'), { ssr: false });
 const Loading = ({ className }: LoadingProps) => {
   return (
     <div className={cn('flex items-center justify-center h-full', className)}>
