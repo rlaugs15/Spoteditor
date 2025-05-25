6d73bcdb28d906c59f1720d5a2efeddd20471579
hayanLee <hayanlee.dev@gmail.com>
2025-05-25 17:53:03 +0900

    style : 이미지 Dialog 구현

--- src/components/features/detail-log/PlaceImageSlider.tsx
+++ src/components/features/detail-log/PlaceImageSlider.tsx
@@ -1,35 +1,68 @@
 'use client';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog';
 import { Tables } from '@/types/supabase';
 import { getStoragePublicImage } from '@/utils/getStorageImage';
 import Image from 'next/image';
+import { useState } from 'react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import { FreeMode } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
 interface PlaceImageSliderProps {
   placeImages: Tables<'place_images'>[];
 }
 
 const PlaceImageSlider = ({ placeImages }: PlaceImageSliderProps) => {
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
   return (
-    <Swiper modules={[FreeMode]} slidesPerView="auto" freeMode className="w-full" spaceBetween={15}>
-      {placeImages
-        .sort((a, b) => a.order - b.order)
-        .map((img) => (
-          <SwiperSlide
-            key={img.place_image_id}
-            className="w-[40vw] max-w-[300px] aspect-[3/4] relative"
-          >
-            <Image
-              src={getStoragePublicImage(img.image_path as string)}
-              alt="장소 이미지"
-              fill
-              className="object-cover"
-            />
-          </SwiperSlide>
-        ))}
-    </Swiper>
+    <>
+      <Swiper
+        modules={[FreeMode]}
+        slidesPerView="auto"
+        freeMode
+        className="w-full"
+        spaceBetween={15}
+      >
+        {placeImages
+          .sort((a, b) => a.order - b.order)
+          .map((img) => (
+            <SwiperSlide
+              key={img.place_image_id}
+              className="w-[40vw] max-w-[300px] aspect-[3/4] relative"
+              onClick={() => setSelectedImage(img.image_path)}
+            >
+              <Image
+                src={getStoragePublicImage(img.image_path as string)}
+                alt="장소 이미지"
+                fill
+                className="object-cover"
+              />
+            </SwiperSlide>
+          ))}
+      </Swiper>
+
+      <Dialog open={!!selectedImage} onOpenChange={() => setSelectedImage(null)}>
+        <DialogContent className=" bg-transparent p-0 flex justify-center items-center border-0 w-full h-1/2 focus-visible:outline-none">
+          <DialogHeader>
+            <DialogTitle hidden>장소 이미지</DialogTitle>
+            <DialogDescription hidden>확대한 장소 이미지</DialogDescription>
+          </DialogHeader>
+          <Image
+            src={getStoragePublicImage(selectedImage as string)}
+            alt="장소 이미지"
+            fill
+            className="object-contain"
+          />
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
 
--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -1,10 +1,10 @@
 'use client';
 
-import * as React from 'react';
 import * as DialogPrimitive from '@radix-ui/react-dialog';
-import { XIcon } from 'lucide-react';
+import * as React from 'react';
 
 import { cn } from '@/lib/utils';
+import { X } from 'lucide-react';
 
 function Dialog({ ...props }: React.ComponentProps<typeof DialogPrimitive.Root>) {
   return <DialogPrimitive.Root data-slot="dialog" {...props} />;
@@ -55,10 +55,10 @@ function DialogContent({
         {...props}
       >
         {children}
-        {/* <DialogPrimitive.Close className=" data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4">
-          <XIcon />
+        <DialogPrimitive.Close className=" data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4">
+          <X className="stroke-white size-6" />
           <span className="sr-only">Close</span>
-        </DialogPrimitive.Close> */}
+        </DialogPrimitive.Close>
       </DialogPrimitive.Content>
     </DialogPortal>
   );
