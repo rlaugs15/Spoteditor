8885b1dfa65a26127de293cbd37986b58e492866
hayanLee <hayanlee.dev@gmail.com>
2025-05-25 17:55:11 +0900

    fix : params에 await 추가

--- src/app/(write)/[logId]/edit/page.tsx
+++ src/app/(write)/[logId]/edit/page.tsx
@@ -2,7 +2,7 @@ import { fetchLog } from '@/app/actions/log';
 import LogEditPage from './_EditPage/page';
 
 const LogEditServerPage = async ({ params }: { params: { logId: string } }) => {
-  const { logId } = params;
+  const { logId } = await params;
   const { data: logData } = await fetchLog(logId);
 
   return <LogEditPage logData={logData} />;
