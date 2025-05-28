974266dfc77d0e183e1dcc5591ce3a9d0ed7b942
hayanLee <hayanlee.dev@gmail.com>
2025-05-28 17:22:24 +0900

    fix : PostCard 로그 상세 페이지 Link 연결
    - 북마크 에러떠서 주석처리

--- src/components/common/Card/PostCard/PostCard.tsx
+++ src/components/common/Card/PostCard/PostCard.tsx
@@ -1,13 +1,14 @@
 import { LogWithUserAndAddress } from '@/types/api/common';
-import dynamic from 'next/dynamic';
+import Link from 'next/link';
+import LogBookMarkButton from '../../Button/Bookmark/LogBookMarkButton';
 import PostCardImage from './PostCardImage';
 import PostCardLocation from './PostCardLocation';
 import PostCardTitle from './PostCardTitle';
 
-const LogBookMarkButton = dynamic(
-  () => import('@/components/common/Button/Bookmark/LogBookMarkButton'),
-  { ssr: false }
-);
+// const LogBookMarkButton = dynamic(
+//   () => import('@/components/common/Button/Bookmark/LogBookMarkButton'),
+//   { ssr: false }
+// );
 
 interface PostCardProps {
   log: LogWithUserAndAddress;
@@ -18,17 +19,19 @@ interface PostCardProps {
 const PostCard = ({ log, vertical, modal }: PostCardProps) => {
   return (
     <div className="cursor-pointer relative">
-      <PostCardImage
-        author={String(log?.users?.nickname)}
-        imageUrl={log?.thumbnail_url}
-        vertical={vertical}
-      />
-      <PostCardTitle title={String(log?.title)} modal={modal} />
-      <PostCardLocation
-        city={log?.address[0]?.city}
-        sigungu={log?.address[0]?.sigungu}
-        modal={modal}
-      />
+      <Link href={`log/${log?.log_id}`}>
+        <PostCardImage
+          author={String(log?.users?.nickname)}
+          imageUrl={log?.thumbnail_url}
+          vertical={vertical}
+        />
+        <PostCardTitle title={String(log?.title)} modal={modal} />
+        <PostCardLocation
+          city={log?.address[0]?.city}
+          sigungu={log?.address[0]?.sigungu}
+          modal={modal}
+        />
+      </Link>
       <LogBookMarkButton logId={String(log?.log_id)} modal={modal} />
     </div>
   );
