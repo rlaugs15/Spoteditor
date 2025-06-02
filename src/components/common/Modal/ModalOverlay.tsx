140958d34226c96f5ee04a49fd921c5b9cd867fd
hayanLee <hayanlee.dev@gmail.com>
2025-06-02 17:26:27 +0900

    style : ModalOverlay w,h 수정

--- src/components/common/Modal/ModalOverlay.tsx
+++ src/components/common/Modal/ModalOverlay.tsx
@@ -9,7 +9,7 @@ export default function ModalOverlay({ children, className }: ModalOverlayProps)
   return (
     <div
       className={cn(
-        'backdrop-blur-[10px] bg-black/50 w-screen h-screen absolute top-0 left-0 z-60 web:flex web:justify-center web:items-center',
+        'backdrop-blur-[10px] bg-black/50 w-dvw h-dvh absolute top-0 left-0 z-60 web:flex web:justify-center web:items-center',
         className
       )}
     >
