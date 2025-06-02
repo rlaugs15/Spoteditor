5fe0f99d9042d9d5839c02b3971adecbb12d7259
hayanLee <hayanlee.dev@gmail.com>
2025-06-02 20:50:30 +0900

    style : 클립보드 alert toast로 변경

--- src/components/common/Button/ClipboardButton.tsx
+++ src/components/common/Button/ClipboardButton.tsx
@@ -1,13 +1,14 @@
 'use client';
 import ExtraActionButton from '@/components/features/detail-log/ExtraActionButton';
+import { toast } from 'sonner';
 import { ShareIcon } from '../Icons';
 
 const copyUrlToClipboard = () => {
   navigator.clipboard
     .writeText(window.location.href)
-    .then(() => alert('URL이 클립보드에 복사되었습니다!'))
+    .then(() => toast.success('URL이 클립보드에 복사되었습니다!'))
     .catch((err) => {
-      alert('URL 복사에 실패했습니다.');
+      toast.error('URL 복사에 실패했습니다.');
       console.error('클립보드 복사 실패:', err);
     });
 };
