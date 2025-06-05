0ab8c2383ddba73cce1e9930fafd7f29a3a66924
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-06-05 19:03:50 +0900

    style: 북마크 버튼 호버 시 배경만 딤 처리되도록 수정

--- src/components/common/Button/Bookmark/PlaceBookMarkButton.tsx
+++ src/components/common/Button/Bookmark/PlaceBookMarkButton.tsx
@@ -1,5 +1,4 @@
 'use client';
-import { Button } from '@/components/ui/button';
 import usePlaceBookmarkMutation from '@/hooks/mutations/place/usePlaceBookmarkMutation';
 import usePlaceBookmarkCheck from '@/hooks/queries/place/useIsPlaceBookmarkCheck';
 import useUser from '@/hooks/queries/user/useUser';
@@ -30,17 +29,15 @@ export default function PlaceBookMarkButton({ placeId, modal }: PlaceBookMarkBut
     });
   };
   return (
-    <Button
-      variant={'outline'}
-      size={'icon'}
+    <button
       onClick={onBookMarkClick}
       disabled={isLoading}
       className={cn(
-        'w-[42px] h-[42px] flex justify-center items-center absolute top-[10px] web:top-[15px] right-[10px] web:right-[15px] hover:brightness-90 rounded-none border-0',
+        'w-[42px] h-[42px] bg-white flex justify-center items-center absolute top-[10px] web:top-[15px] right-[10px] web:right-[15px] rounded-none border-0',
         modal && 'top-1.5 right-1.5'
       )}
     >
       <Bookmark className={cn('!w-6 !h-6', data?.isBookmark && 'fill-black')} />
-    </Button>
+    </button>
   );
 }
