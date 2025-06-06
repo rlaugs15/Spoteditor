e6850d95582624042609890af9c0cc62fc75ac02
hayanLee <hayanlee.dev@gmail.com>
2025-06-06 16:20:59 +0900

    fix : writeLayout을 벗어나면 전역상태로 저장하는 태그 초기화

--- src/app/(write)/layout.tsx
+++ src/app/(write)/layout.tsx
@@ -1,6 +1,14 @@
-import { PropsWithChildren } from 'react';
+'use client';
+import { useLogCreationStore } from '@/stores/logCreationStore';
+import { PropsWithChildren, useEffect } from 'react';
 
 const WriteLayout = ({ children }: PropsWithChildren) => {
+  const clearTag = useLogCreationStore((state) => state.clearTag);
+  useEffect(() => {
+    return () => clearTag();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className="flex flex-col max-w-[724px] mx-auto h-dvh font-pretendard">
       <div className="grow px-4 bg-white">{children}</div>
