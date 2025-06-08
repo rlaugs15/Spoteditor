86759569648b036330a439ac61dc8e559263c2e0
hayanLee <hayanlee.dev@gmail.com>
2025-06-09 01:51:01 +0900

    move : public 이미지 가져오는 유틸함수 하나의 파일로 합침

--- src/utils/buildPublicUrl.ts
+++
@@ -1,6 +0,0 @@
-import { StorageBucket } from '@/types/api/storage';
-
-/* Public URL 생성 */
-export function buildPublicUrl(bucket: StorageBucket, path: string) {
-  return `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/${bucket}/${path}`;
-}
--- src/utils/getStorageImage.ts
+++ src/utils/getStorageImage.ts
@@ -1,2 +1,8 @@
+import { StorageBucket } from '@/types/api/storage';
+
 export const getStoragePublicImage = (storedPath: string) =>
   `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/${storedPath}`;
+
+export function buildPublicUrl(bucket: StorageBucket, path: string) {
+  return `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/${bucket}/${path}`;
+}
