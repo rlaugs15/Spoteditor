aee5277de0d6be7ad0e54bbd660133d3ae05b8fd
hayanLee <hayanlee.dev@gmail.com>
2025-06-10 01:02:00 +0900

    fix : 상수 객체로 페이지별 태그 선택 영역에 보여주는 인트로 변경되도록 개선

---
+++ src/components/features/log/register/tags/constant.ts
@@ -0,0 +1,31 @@
+export const PAGE_TAG_INTROS = {
+  mood: {
+    title: '어떤 하루인가요?',
+    des: '여러개를 선택할 수 있어요 (최대 6개)',
+  },
+  country: { title: '국가를 선택해주세요.', des: '메인 국가를 선택해주세요.' },
+  city: {
+    domestic: {
+      title: '도시를 선택해주세요.',
+      des: '메인 도시를 선택해주세요.',
+      tagGroupTitle: '도시',
+    },
+    aboard: {
+      title: '지역을 선택해주세요.',
+      des: '메인 지역을 선택해주세요.',
+      tagGroupTitle: '지역',
+    },
+  },
+  sigungu: {
+    domestic: {
+      title: '시/군/구를 선택해주세요.',
+      des: '메인 시/군/구를 선택해주세요.',
+      tagGroupTitle: '시/군/구',
+    },
+    aboard: {
+      title: '도시를 선택해주세요.',
+      des: '메인 도시를 선택해주세요.',
+      tagGroupTitle: '도시',
+    },
+  },
+};
