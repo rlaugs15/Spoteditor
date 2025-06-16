f388830fd6f51429e69c035159de6d21f8c07025
hayanLee <hayanlee.dev@gmail.com>
2025-06-16 22:47:57 +0900

    fix : hero ui 수정

--- src/components/features/home/Hero.tsx
+++ src/components/features/home/Hero.tsx
@@ -3,14 +3,14 @@ import CitySearchForm from './CitySearchForm/CitySearchForm';
 const Hero = () => {
   return (
     <div className="bg-black px-4 web:px-[50px] py-[30px] web:py-10 flex flex-col web:grid web:grid-cols-2 justify-between gap-[25px]">
-      <div className="flex text-white text-md font-medium web:text-4xl items-center leading-[1.4] shrink-0 tracking-tight">
+      <div className="flex text-white text-md font-medium web:text-[34px] items-center leading-[1.3] shrink-0 tracking-tight">
         어디 갈지 고민될 땐?
         <br />
         감각있는 스팟 큐레이터들이 직접 만든
         <br />
         코스를 만나보세요!
       </div>
-      <div className="flex flex-col justify-center gap-[25px] web:px-5">
+      <div className="flex flex-col justify-center gap-[25px]">
         <CitySearchForm />
         {/* <HeroCategoryList /> */}
       </div>
