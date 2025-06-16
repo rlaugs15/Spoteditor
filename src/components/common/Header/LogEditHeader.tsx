5cde90f651ca7586c7d3604f343dbbff9cecc202
hayanLee <hayanlee.dev@gmail.com>
2025-06-16 21:52:56 +0900

    fix : 로그 수정 페이지 헤더 fixed로 변경

--- src/components/common/Header/LogEditHeader.tsx
+++ src/components/common/Header/LogEditHeader.tsx
@@ -10,14 +10,16 @@ interface LogEditHeaderProps {
 
 const LogEditHeader = ({ city, sigungu, logTitle, logId }: LogEditHeaderProps) => {
   return (
-    <header className="py-[15px] bg-white flex items-center justify-between">
-      <div className="flex items-center gap-2.5">
-        <BackButton plain />
-        <p className="text-text-2xl font-bold">
-          {city} · {sigungu}
-        </p>
+    <header className="fixed top-0 left-0 right-0 z-50 h-[54px] bg-white">
+      <div className="py-[15px] flex items-center justify-between w-full min-w-[343px] max-w-[724px] mx-auto px-4">
+        <div className="flex items-center gap-2.5">
+          <BackButton plain />
+          <p className="text-text-2xl font-bold">
+            {city} · {sigungu}
+          </p>
+        </div>
+        <ConfirmDeleteDialog logTitle={logTitle} logId={logId} />
       </div>
-      <ConfirmDeleteDialog logTitle={logTitle} logId={logId} />
     </header>
   );
 };
