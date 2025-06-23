a8950462bce5a6e7f1f6aa57f73d2d0db4c7933c
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-06-24 04:55:07 +0900

    feat: 프로필 편집 번역 기능 추가

--- src/components/features/profile-editor/ProfileFormFields.tsx
+++ src/components/features/profile-editor/ProfileFormFields.tsx
@@ -2,61 +2,69 @@
 
 import { FormControl, FormField, FormItem, FormLabel } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import { useTranslations } from 'next-intl';
 import { useFormContext } from 'react-hook-form';
 import TextCounter from './TextCounter';
 
 export default function ProfileFormFields() {
   const { control, watch } = useFormContext();
   const { nickname: nicknameLeng, description: descLeng } = watch();
+
+  const t = useTranslations('ProfileEditor');
   return (
     <section>
-      <p className="mt-8 mb-4 font-bold text-text-md web:text-text-2xl">프로필 편집</p>
+      <p className="mt-8 mb-4 font-bold text-text-md web:text-text-2xl">{t('title')}</p>
       <div className="flex flex-col gap-5">
+        {/* 닉네임 */}
         <FormField
           control={control}
           name="nickname"
           render={({ field }) => (
             <FormItem className="border-b-[1px] border-b-primary-100">
               <div className="flex items-center justify-between py-[5px]">
-                <FormLabel className="font-bold text-text-sm">닉네임</FormLabel>
+                <FormLabel className="font-bold text-text-sm"> {t('title')}</FormLabel>
                 <TextCounter length={nicknameLeng.length} maxLength={30} />
               </div>
               <FormControl>
                 <Input
                   {...field}
-                  placeholder="닉네임"
+                  placeholder={t('nickname.label')}
                   className="!text-text-sm text-light-500 font-medium placeholder:text-text-sm py-[5px] px-0"
                 />
               </FormControl>
             </FormItem>
           )}
         />
+
+        {/* 프로필 설명 */}
         <FormField
           control={control}
           name="description"
           render={({ field }) => (
             <FormItem className="border-b-[1px] border-b-primary-100">
               <div className="flex items-center justify-between py-[5px]">
-                <FormLabel className="font-bold text-text-sm">프로필 설명</FormLabel>
+                <FormLabel className="font-bold text-text-sm">{t('description.label')}</FormLabel>
                 <TextCounter length={descLeng.length} maxLength={50} />
               </div>
               <FormControl>
                 <Input
                   {...field}
-                  placeholder="프로필 설명을 입력하세요."
+                  placeholder={t('description.placeholder')}
                   className="!text-text-sm text-light-500 font-medium placeholder:text-text-sm py-[5px] px-0"
                 />
               </FormControl>
             </FormItem>
           )}
         />
+
+        {/* 인스타그램 */}
         <FormField
           control={control}
           name="insta_id"
           render={({ field }) => (
             <FormItem className="border-b-[1px] border-b-primary-100">
               <div className="flex items-center justify-start py-[5px]">
-                <FormLabel className="font-bold text-text-sm">인스타그램</FormLabel>
+                <FormLabel className="font-bold text-text-sm">{t('instagram.label')}</FormLabel>
               </div>
               <FormControl>
                 <Input
