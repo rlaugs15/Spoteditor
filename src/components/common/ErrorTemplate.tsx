e052ee24b094c7696cf9544471fd6a7969bda940
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-06-30 04:46:46 +0900

    feat: NotFound 페이지 다국어 처리

--- src/components/common/ErrorTemplate.tsx
+++ src/components/common/ErrorTemplate.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from '@/components/ui/button';
 import { useRouter } from '@/i18n/navigation';
+import { useTranslations } from 'next-intl';
 import Image from 'next/image';
 interface ErrorTemplateProps {
   title?: string;
@@ -13,13 +14,14 @@ export default function ErrorTemplate({
   message = 'URL 주소를 확인해주세요.',
 }: ErrorTemplateProps) {
   const router = useRouter();
+  const t = useTranslations('NotFoundPage');
   const handleClick = () => router.replace('/');
   return (
     <main className="flex flex-col items-center justify-center h-dvh grow">
       <Image src="/images/404.png" alt="404" width={300} height={200} />
       <div className="flex flex-col items-center mt-[50px] mb-5">
-        <h4 className="text-text-xl font-bold">{title}</h4>
-        <h5 className="text-center">{message}</h5>
+        <h4 className="text-text-xl font-bold">{title ?? t('title')}</h4>
+        <h5 className="text-center">{message ?? t('defaultMessage')}</h5>
       </div>
       <Button className="rounded-full mt-5" size={'lg'} onClick={handleClick}>
         홈으로 이동
