9f8c3f5b1d6e422736ecbae01da03555bf28eefa
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-07-02 15:23:35 +0900

    fix: 핫자 작동 안 되는 현상 수정

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -60,20 +60,22 @@ export default async function RootLayout({
           }}
         />
         {/* Hotjar */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-      (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:${process.env.NEXT_PUBLIC_HOTJAR_ID},hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-    `,
-          }}
-        />
+        {process.env.NODE_ENV === 'production' && process.env.NEXT_PUBLIC_HOTJAR_ID && (
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+        (function(h,o,t,j,a,r){
+          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+          h._hjSettings={hjid:${JSON.stringify(process.env.NEXT_PUBLIC_HOTJAR_ID)},hjsv:6};
+          a=o.getElementsByTagName('head')[0];
+          r=o.createElement('script');r.async=1;
+          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+          a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+      `,
+            }}
+          />
+        )}
       </head>
       <body
         className={`${pretendard.variable} ${untitled.variable} ${prompt.variable} antialiased`}
