07e9a0aef13cf1b308173f1a1a62e1a9d4d79f0c
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-07-08 16:23:27 +0900

    style: 홈 화면 등록 버튼 호버 시 생상 수정

--- src/components/features/home/InfoBanner/RegisterLinkButtons.tsx
+++ src/components/features/home/InfoBanner/RegisterLinkButtons.tsx
@@ -13,13 +13,13 @@ export default function RegisterLinkButtons({ label }: RegisterLinkButtonsProps)
   return (
     <div className="flex items-center">
       <Button
-        className="rounded-full w-56 h-15 !text-text-sm web:!text-text-md font-medium bg-white text-black"
+        className="rounded-full w-56 h-15 !text-text-sm web:!text-text-md font-medium bg-white text-black hover:bg-white hover:text-black"
         size="lg"
         asChild
       >
         <Link href={REGISTER_PATHS.MOOD}>{label}</Link>
       </Button>
-      <Button className="rounded-full w-15 h-15 bg-white" size="icon" asChild>
+      <Button className="rounded-full w-15 h-15 bg-white hover:bg-white" size="icon" asChild>
         <Link href={REGISTER_PATHS.MOOD}>
           <ArrowUpRightIcon className="w-9 h-9" />
         </Link>
