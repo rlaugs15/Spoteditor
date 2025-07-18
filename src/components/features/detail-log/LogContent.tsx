c917e655f4ddc3e2dd97f07da580578a7620cc05
daylogowner <daylogowner@gmail.com>
2025-07-18 13:37:35 +0900

    로그 상세 로케이션 아이콘 상 정렬

--- src/components/features/detail-log/LogContent.tsx
+++ src/components/features/detail-log/LogContent.tsx
@@ -32,8 +32,8 @@ const LogContent = ({ place, idx }: LogContentProps) => {
             <MapIcon className="w-4.5 h-4.5" />
             <span className="break-words block min-w-0">{t(`${place.category}`)}</span>
           </div>
-          <div className="flex items-center gap-1.5 text-light-400 text-text-sm web:text-text-lg">
-            <LocationIcon className="w-4.5 h-4.5 flex-shrink-0" />
+          <div className="flex items-start gap-1.5 text-light-400 text-text-sm web:text-text-lg">
+            <LocationIcon className="w-4.5 h-4.5 flex-shrink-0  mt-[4px]" />
             <span className="break-words block min-w-0">{place.address}</span>
           </div>
         </div>
