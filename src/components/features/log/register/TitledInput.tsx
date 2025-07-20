c0b2b056c90812c5837fe3e8719bc761781b8f80
daylogowner <daylogowner@gmail.com>
2025-07-20 17:00:57 +0900

    로그 작성하기 타이틀 인풋필드 수정

--- src/components/features/log/register/TitledInput.tsx
+++ src/components/features/log/register/TitledInput.tsx
@@ -13,37 +13,44 @@ const TitledInput = () => {
   const t = useTranslations('Register.LogPage');
 
   return (
-    <div className="relative flex items-center mt-4">
-      <FormField
-        control={control}
-        name="logTitle"
-        render={({ field }) => (
-          <>
-            <Input
-              {...field}
-              type="text"
-              placeholder={`${t('titlePlaceholder')} *`}
-              className={cn(
-                'block w-full px-4 py-6 rounded-[8px] bg-light-50 text-black',
-                'placeholder:text-light-300 !text-[14px] focus:outline-none',
-                isError && 'placeholder:text-error-500'
-              )}
-              maxLength={30}
-              required
-            />
-            <button
-              type="button"
-              className={cn(
-                'absolute right-2 p-2 transition-opacity duration-300',
-                logTitle ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'
-              )}
-              onClick={() => setValue('logTitle', '')}
-            >
-              <XInputClearIcon className="cursor-pointer hover:brightness-95" />
-            </button>
-          </>
-        )}
-      />
+    // <div className="relative flex items-center mt-4">
+    <div className="space-y-1 w-full">
+      <label className="block text-[14px] font-semibold text-black">
+        {t('titleLabel')}
+        <span className="text-error-500"> *</span>
+      </label>
+      <div className="relative">
+        <FormField
+          control={control}
+          name="logTitle"
+          render={({ field }) => (
+            <>
+              <Input
+                {...field}
+                type="text"
+                placeholder={`${t('titlePlaceholder')} *`}
+                className={cn(
+                  'block w-full px-4 py-6 rounded-[8px] bg-light-50 text-black',
+                  'placeholder:text-light-300 !text-[14px] focus:outline-none',
+                  isError && 'placeholder:text-error-500'
+                )}
+                maxLength={30}
+                required
+              />
+              <button
+                type="button"
+                className={cn(
+                  'absolute top-1/2 -translate-y-1/2 right-2 p-2 transition-opacity duration-300',
+                  logTitle ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'
+                )}
+                onClick={() => setValue('logTitle', '')}
+              >
+                <XInputClearIcon className="cursor-pointer hover:brightness-95" />
+              </button>
+            </>
+          )}
+        />
+      </div>
     </div>
   );
 };
