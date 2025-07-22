d2642e8a115a475e14ce63763e5bca2d2febbacc
daylogowner <daylogowner@gmail.com>
2025-07-22 11:24:45 +0900

    메인 상단 수정

--- src/components/features/home/InfoBanner/TypingText.tsx
+++ src/components/features/home/InfoBanner/TypingText.tsx
@@ -7,11 +7,11 @@ const TypingText = () => {
       sequence={[
         '여행지에서, 익숙한 동네에서—\n당신의 취향이 머무는 장소로 하루를 그려보세요',
         1200,
-        'Traveling or staying local—\ncreate your day with the places you love.',
+        'Traveling or staying local—\ncreate your day with the places you love',
         1200,
       ]}
       speed={40}
-      className="font-bold text-md web:text-xl text-black whitespace-pre-line"
+      className="font-bold text-sm web:text-xl text-black whitespace-pre-line"
       repeat={Infinity}
     />
   );
