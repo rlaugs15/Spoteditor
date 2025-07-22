f502091ec4546e28ad2436afd78ab6f3b83d7356
daylogowner <daylogowner@gmail.com>
2025-07-22 10:56:40 +0900

    main 다시 재시도 테스트

--- src/components/features/home/InfoBanner/TypingText.tsx
+++ src/components/features/home/InfoBanner/TypingText.tsx
@@ -5,7 +5,7 @@ const TypingText = () => {
   return (
     <TypeAnimation
       sequence={[
-        '여행지에서, 익숙한 동네에서—\n당신의 취향이 머무는 장소로 하루를 그려보세요.',
+        '여행지에서, 익숙한 동네에서—\n당신의 취향이 머무는 장소로 하루를 그려보세요',
         1200,
         'Traveling or staying local—\ncreate your day with the places you love.',
         1200,
