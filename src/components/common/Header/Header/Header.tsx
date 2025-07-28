33e7385008dff13f37d00926d7d17698ccca8483
daylogowner <daylogowner@gmail.com>
2025-07-28 21:40:52 +0900

    웹 헤더 아이콘 갭 수정

--- src/components/common/Header/Header/Header.tsx
+++ src/components/common/Header/Header/Header.tsx
@@ -11,7 +11,7 @@ const Header = async () => {
   return (
     <header className="flex items-center justify-between fixed w-full z-50 bg-black px-4 web:px-[50px] py-4 web:py-5 left-0 top-0 web:h-[60px] h-12">
       <Logo />
-      <section className="flex items-center text-white web:gap-11">
+      <section className="flex items-center text-white web:gap-8">
         <SearchBarButton />
         <Popover>
           <PopoverTrigger asChild>
