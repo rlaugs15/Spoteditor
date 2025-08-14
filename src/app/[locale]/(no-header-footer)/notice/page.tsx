3c446fa539d0d43e5600059f072a45335a13079e
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-08-15 03:02:53 +0900

    feat: 로그인 및 공지사항 관련 페이지에 메타데이터 추가

--- src/app/[locale]/(no-header-footer)/notice/page.tsx
+++ src/app/[locale]/(no-header-footer)/notice/page.tsx
@@ -1,8 +1,13 @@
 import NoticeHeader from '@/components/features/notice/NoticeHeader';
+import { Metadata } from 'next';
 import { getTranslations } from 'next-intl/server';
 
+export const metadata: Metadata = {
+  title: 'notice',
+};
+
 export default async function NoticePage() {
-  const t = await getTranslations('NoticePage')
+  const t = await getTranslations('NoticePage');
   return (
     <main className="flex justify-center w-full">
       <div className="max-w-[724px] w-full">
